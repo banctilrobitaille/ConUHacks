67943447209e617214d68e97ccdcecc1d608de46
Benoit Anctil-Robitaille <benoit.robitaille@hotmail.ca>
2016-01-23 11:18:21 -0500

    Adding CSS stylesheets and index template

--- IntelligentCellar/views.py
+++ IntelligentCellar/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.template import RequestContext
+from django.shortcuts import render_to_response
 # Create your views here.
+
+
+def index(request):
+
+    context_dict = {}
+
+
+    return render_to_response('IntelligentCellar/index.html',context_dict)
